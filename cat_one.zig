ecdca509ca297390922840d4882019fe0595c909
opeyemi <ofolorunsho@influxdata.com>
2026-02-08 17:29:35 +0000

    chore: fix issue

--- cat_one.zig
+++ cat_one.zig
@@ -5,15 +5,15 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const ally = gpa.allocator();
 
-    const args = std.process.argsAlloc(ally);
+    const args = try std.process.argsAlloc(ally);
     defer std.process.argsFree(ally, args);
 
     var cwd = std.fs.cwd();
     const max_bytes = 16 * 1024 * 1024;
 
     var stdoutbuffer: [1024]u8 = undefined;
-    const stdout = std.fs.File.stdout().writer(&stdoutbuffer);
-    const stdo = &stdout.interface;
+    var stdout = std.fs.File.stdout().writer(&stdoutbuffer);
+    var stdo = &stdout.interface;
 
     for (args[1..]) |file_path| {
         const text = try cwd.readFileAlloc(ally, file_path, max_bytes);
